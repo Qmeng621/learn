9d4ba69021556ac3428493e024901864798d3e65
Qmeng621 <1244729374@qq.com>
2017-04-16 22:39:18 +0800

    learn 再次提交

---
+++ javescript/course-07/index.js
@@ -0,0 +1,10 @@
+var my_a = [1,2,4,6,8];
+//  console.log(my_a.indexOf(4));
+ function my(){
+	 var key;
+	 var i = 0;
+	 if(i < my_a.length){
+		 my_a[i] = key;
+		 console.log(my_a.indexOf(4))
+	 }
+ }
